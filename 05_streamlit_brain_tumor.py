3ee0a964fd0869cc1bc4be24295bffb18b156c41
Zayan Ahmad Ghous <zayanahmad851@gmail.com>
2025-07-19 18:41:14 +0500

    Initial commit

--- 05_streamlit_brain_tumor.py
+++ 05_streamlit_brain_tumor.py
@@ -20,6 +20,26 @@ def preprocess_image(img):
     img_array = img_array.reshape(1, 150, 150, 1) / 255.0
     return img_array
 
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# Rebuild architecture
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(150,150,1)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dropout(0.3),
+    Dense(2, activation='softmax')
+])
+
+# Load weights
+model.load_weights("model.weights.h5")
+
 # Predict
 model = tf.keras.models.load_model("model.keras")
 
